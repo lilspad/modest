492e2250d42db50d5a6fdd5cbd25840e38da2ac5
lilspad <lilypaczesniak@outlook.com>
2023-02-22 23:54:23 +0000

    render carousel comp instead of img

--- src/components/product.js
+++ src/components/product.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Carousel from './carousel.js';
+
+
 
 class Product extends React.Component {
 
     render() {
         return (
             <div className="product">
-                <img src={this.props.imgSrc} alt={this.props.productName + "image"} />
+                <Carousel />
                 <div className="product-header">
                     <h3>{this.props.productName}</h3>
                     <p>£{this.props.productPrice} | {this.props.productSize} </p>
                 </div>
-                <p>{this.props.productDescription}</p>
+                <p className="description">{this.props.productDescription}</p>
                 <div className="product-buttons">
-                    <button>Add to basket</button>
-                    <button>Favourite</button>
+                    <button className="product-button">Add to basket</button>
+                    <button className="product-button">Favourite</button>
                 </div>
             </div>
         )

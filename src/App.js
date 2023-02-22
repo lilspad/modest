53525d361697242bd31db512097d198dd8c79b9e
lilspad <lilypaczesniak@outlook.com>
2023-02-22 21:06:56 +0000

    initial set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="brand-logo">Modest</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

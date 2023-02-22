ae8c0f713d5fbc92ab774ea14eccac8eb316671d
lilspad <lilypaczesniak@outlook.com>
2023-02-22 23:56:35 +0000

    add src and alt attributes for slide images

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -6,9 +6,9 @@ export default function Carousel(props) {
         <div className="carousel">
             <section className="slider-wrapper">
             <div className="slides-container" id="slides-container">
-                <div className="img-container slide"><img src={""} /></div>
-                <div className="img-container slide"><img src={""} /></div>
-                <div className="img-container slide"><img src={""} /></div>
+                <div className="img-container slide"><img src={props.productSrc[0].src} alt={props.productSrc[0].alt} /></div>
+                <div className="img-container slide"><img src={props.productSrc[1].src} alt={props.productSrc[1].alt} /></div>
+                <div className="img-container slide"><img src={props.productSrc[2].src} alt={props.productSrc[2].alt} /></div>
             </div>
             <button className="slide-arrow" id="slide-arrow-prev" onClick={ () => {}}> {'<'}
             </button>

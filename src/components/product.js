0d22d9ed52097f70eeaa2a8e869eee47a1e7cc6e
lilspad <lilypaczesniak@outlook.com>
2023-02-22 23:54:57 +0000

    create media comp for the carousel

--- src/components/product.js
+++ src/components/product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from './carousel.js';
-
+import {tonic} from '../media/product-media.js';
 
 
 class Product extends React.Component {
@@ -8,7 +8,7 @@ class Product extends React.Component {
     render() {
         return (
             <div className="product">
-                <Carousel />
+                <Carousel productSrc={tonic} />
                 <div className="product-header">
                     <h3>{this.props.productName}</h3>
                     <p>£{this.props.productPrice} | {this.props.productSize} </p>
---
+++ src/media/product-media.js
@@ -0,0 +1,20 @@
+import tonic1 from './products/tonic1.jpg';
+import tonic2 from './products/tonic2.jpg';
+import tonic3 from './products/tonic3.jpg';
+
+const tonic = [
+    {
+        alt: "Product image of tonic",
+        src: tonic1
+    },
+    {
+        alt: "A woman holding the product close up to camera",
+        src: tonic2
+    },
+    {
+        alt: "A woman pouring the tonic onto a cotton pad",
+        src: tonic3
+    }
+];
+
+export {tonic};

109b3d78b740abe6bcd54210afde6aa441c0f91e
lilspad <lilypaczesniak@outlook.com>
2023-02-23 02:13:00 +0000

    add some favourite functionality

--- src/components/product.js
+++ src/components/product.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Carousel from './carousel.js';
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favourited: false
+        };
+    }
 
     render() {
+        const productId = "product" + this.props.productId;
         return (
-            <div className="product">
+            <div className="product" id={productId}>
                 <Carousel productSrc={this.props.productSrc} productId={this.props.productId}/>
                 <div className="product-header">
                     <h3>{this.props.productName}</h3>
@@ -13,10 +20,18 @@ class Product extends React.Component {
                 <p className="description">{this.props.productDescription}</p>
                 <div className="product-buttons">
                     <button className="product-button"> <i className="fa-solid fa-basket-shopping"></i> Add to basket</button>
-                    <button className="product-button"> <i className="fa-solid fa-heart"></i> Favourite</button>
+                    <button className="product-button favourite" onClick={() => this.handleFavourite(productId)}> <i className="fa-solid fa-heart"></i> Favourite</button>
                 </div>
             </div>
         )
+    }
+
+    handleFavourite(id) {
+        
+        this.setState({
+            favourited: true
+        })
+        document.getElementById(id).classList.add("favourited");
     }
 }
 

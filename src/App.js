8f808cf9bb8de8457485849ce6e74a7d4f25b61e
lilspad <lilypaczesniak@outlook.com>
2023-02-23 00:57:11 +0000

    add remaining products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './style.scss';
 import Product from './components/product.js';
+import {cleanser, serum, tonic, oil, moisturiser} from './media/product-media.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="brand-logo">Modest</h1>
-        <div>
+        <div className="header-menu">
           <i className="fa-solid fa-circle-user"></i>
+          <i class="fa-solid fa-heart"></i>
           <i className="fa-solid fa-basket-shopping"></i>
         </div>
       </header>
@@ -16,12 +18,47 @@ function App() {
         
         </div>
         <div className="product-layout">
+
+          <Product 
+            productName="Cleanser" 
+            productPrice={6} 
+            productSize="300ml" 
+            productDescription="Gentle, foaming cleanser designed for all skin types." 
+            productSrc={cleanser}
+          />
+        
           <Product 
             productName="Tonic" 
             productPrice={5} 
             productSize="500ml" 
             productDescription="Refreshing tonic for a clean feel skin." 
-             />
+            productSrc={tonic}
+          />
+
+          <Product 
+            productName="Serum" 
+            productPrice={12} 
+            productSize="50ml" 
+            productDescription="Packed with vitamins and actives, superbly performing serum for all your needs." 
+            productSrc={serum}
+          />
+
+          <Product 
+            productName="Oil" 
+            productPrice={9} 
+            productSize="50ml" 
+            productDescription="Lightweight and non-sticky moisturisiing oil." 
+            productSrc={oil}
+          />
+
+          <Product 
+            productName="Moisturiser" 
+            productPrice={8} 
+            productSize="90ml" 
+            productDescription="Quick-absorbing, all-purpose moisturiser sutiable for all skin types." 
+            productSrc={moisturiser}
+          />
+
         </div>
       </main>
     </div>
--- src/media/product-media.js
+++ src/media/product-media.js
@@ -1,6 +1,37 @@
+import cleanser1 from './products/cleanser1.jpg';
+import cleanser2 from './products/cleanser2.jpg';
+import cleanser3 from './products/cleanser3.jpg';
+
 import tonic1 from './products/tonic1.jpg';
 import tonic2 from './products/tonic2.jpg';
 import tonic3 from './products/tonic3.jpg';
+
+import serum1 from './products/serum1.jpg';
+import serum2 from './products/serum2.jpg';
+import serum3 from './products/serum3.jpg';
+
+import oil1 from './products/oil1.jpg';
+import oil2 from './products/oil2.jpg';
+import oil3 from './products/oil3.jpg';
+
+import cream1 from './products/cream1.jpg';
+import cream2 from './products/cream2.jpg';
+import cream3 from './products/cream3.jpg';
+
+const cleanser = [
+    {
+        alt: "",
+        src: cleanser1
+    },
+    {
+        alt: "",
+        src: cleanser2
+    },
+    {
+        alt: "",
+        src: cleanser3
+    }
+]
 
 const tonic = [
     {
@@ -17,4 +48,49 @@ const tonic = [
     }
 ];
 
-export {tonic};
+const serum = [
+    {
+        alt: "Product image of serum",
+        src: serum1
+    },
+    {
+        alt: "A woman holding the product close up to camera",
+        src: serum2
+    },
+    {
+        alt: "A woman pouring the serum onto a cotton pad",
+        src: serum3
+    }
+];
+
+const oil = [
+    {
+        alt: "Product image of oil",
+        src: oil1
+    },
+    {
+        alt: "",
+        src: oil2
+    },
+    {
+        alt: "",
+        src: oil3
+    }
+]
+
+const moisturiser = [
+    {
+        alt: "Product image of moisturiser",
+        src: cream1
+    },
+    {
+        alt: "A woman holding the product close up to camera",
+        src: cream2
+    },
+    {
+        alt: "A woman pouring the moisturiser onto a cotton pad",
+        src: cream3
+    }
+];
+
+export {cleanser, serum, tonic, oil, moisturiser};

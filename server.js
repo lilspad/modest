3e51770479dfba339e51ea71dd68773b9d34f390
lilspad <lilypaczesniak@outlook.com>
2023-02-27 23:39:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,18 @@ const app = express();
 app.use(express.static('public'));
 
 const YOUR_DOMAIN = 'http://localhost:4242';
-const home = window.location.href
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
         price: 'price_1MgDd0GrxL0zd9YA1UeCn71e',
         quantity: 1,
       },
     ],
     mode: 'payment',
     success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: home,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
 
   res.redirect(303, session.url);

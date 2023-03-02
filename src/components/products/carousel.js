c4d983a3c4e9cb9b5920191d6da14fd2ec42f123
lilspad <lilypaczesniak@outlook.com>
2023-03-02 20:20:37 +0000

    add keys

--- src/components/products/carousel.js
+++ src/components/products/carousel.js
@@ -8,11 +8,9 @@ export default function Carousel(props) {
             <section className="slider-wrapper">
             <div className="slides-container" id="slides-container">
             {//Check if media is loaded (productInfo) and display img array if true, or loading-img if false
-            ProductInfo() ? [(<div className="img-container slide"><img src={props.productSrc[0].src} alt={props.productSrc[0].alt} /></div>),
-                !props.productSrc[1] ? 'nada' : (
-                (<div className="img-container slide"><img src={props.productSrc === defaultMedia ? "" : props.productSrc[1].src} alt={props.productSrc === defaultMedia ? "" : props.productSrc[1].alt} /></div>),
-                (<div className="img-container slide"><img src={props.productSrc === defaultMedia ? "" : props.productSrc[2].src} alt={props.productSrc === defaultMedia ? "" : props.productSrc[2].alt} /></div>)
-                )
+            ProductInfo() ? [(<div className="img-container slide" key="img1"><img src={props.productSrc[0].src} alt={props.productSrc[0].alt} /></div>),
+                (<div className="img-container slide" key="img2"><img src={props.productSrc === defaultMedia ? "" : props.productSrc[1].src} alt={props.productSrc === defaultMedia ? "" : props.productSrc[1].alt}  /></div>),
+                (<div className="img-container slide" key="img3"><img src={props.productSrc === defaultMedia ? "" : props.productSrc[2].src} alt={props.productSrc === defaultMedia ? "" : props.productSrc[2].alt}  /></div>)
             ] : <div className="img-container slide loading-img"></div> }
             </div>
 

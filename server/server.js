a3cfeeee662fdab8f7013e542c2fbfc6de8b76c1
lilspad <lilypaczesniak@outlook.com>
2023-03-21 15:36:16 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
  extended: true})); 
 app.use(cors())
 
-const domain = 'http://localhost:3000';
+const domain = 'https://modestskincare.netlify.app';
 
 app.post('/create-checkout-session', async (req, res) => {
   const prices = JSON.parse(req.body.prices);
